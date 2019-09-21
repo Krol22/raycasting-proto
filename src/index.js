32e23d2afe2aad97f1ca46413ff8c72cc89857d8
Karol Torun <krol22ee@gmail.com>
2019-09-21 22:48:08 +0200

    Refactor - moving DDA to separate function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { map } from './map';
 
-// const MAP_WIDTH = 24;
-// const MAP_HEIGHT = 24;
+const MAP_WIDTH = 24;
+const MAP_HEIGHT = 24;
 //
 const mapValue = (input, a, b, c, d) => {
   return c + ((d - c) / (b - a)) * (input - a);
@@ -40,7 +40,6 @@ const getImageData = image => {
   return ctx.getImageData(0, 0, image.width, image.height);
 };
 
-let textureImageData;
 let floorImageData;
 let celingImageData;
 
@@ -51,15 +50,92 @@ function addPixelToImageData(sourceData, sourceIndex, dest, destIndex, alpha) {
   dest.data[destIndex + 3] = alpha;
 }
 
-let cameraX, rayDirX, rayDirY;
+function DDA(rayDirX, rayDirY) {
+  const hitWalls = [];
+  let mapX = Math.floor(posX);
+  let mapY = Math.floor(posY);
 
+  let sideDistX, sideDistY;
+  let side;
+  let stepX, stepY;
+  const deltaDistX = Math.sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
+  const deltaDistY = Math.sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
+  
+  if (rayDirX < 0) {
+    stepX = -1;
+    sideDistX = (posX - mapX) * deltaDistX;
+  } else {
+    stepX = 1;
+    sideDistX = (mapX + 1 - posX) * deltaDistX;
+  }
+  if (rayDirY < 0) {
+    stepY = -1;
+    sideDistY = (posY - mapY) * deltaDistY;
+  } else {
+    stepY = 1;
+    sideDistY = (mapY + 1 - posY) * deltaDistY;
+  } 
+
+  while (true) {
+    if (sideDistX < sideDistY) {
+      sideDistX += deltaDistX;
+      mapX += stepX;
+      mapX = Math.floor(mapX);
+      side = 0;
+    } else {
+      sideDistY += deltaDistY;
+      mapY += stepY;
+      mapY = Math.floor(mapY);
+      side = 1;
+    }
+
+    if (map[mapX][mapY] > 0) {
+      let newWall = {
+        mapX, mapY, side,
+        value: map[mapX][mapY],
+      };
+
+      let backWallX = mapX;
+      let backWallY = mapY;
+      let backWallSide;
+
+      if (sideDistX < sideDistY) {
+        backWallX += stepX;
+        backWallX = Math.floor(backWallX);
+        backWallSide = 0;
+      } else {
+        backWallY += stepY;
+        backWallY = Math.floor(backWallY);
+        backWallSide = 1;
+      }
+
+      let backWall = {
+        mapX: backWallX,
+        mapY: backWallY,
+        side: backWallSide,
+        value: 3,
+      };
+
+      newWall.backWall = backWall;
+
+      hitWalls.push(newWall);
+
+      if (map[mapX][mapY] !== 3) {
+        break;
+      }
+    }
+  }
+
+  return [hitWalls, stepX, stepY];
+}
+
+let cameraX, rayDirX, rayDirY;
 let image;
 
 let w = 800;
 let h = 400;
 
 const update = () => {
-  // cast 'RAYS';
   for (let x = 0; x < w; x++) {
     cameraX = 2 * x / w - 1; 
 
@@ -67,101 +143,9 @@ const update = () => {
     rayDirX = dirX + planeX * cameraX;
     rayDirY = dirY + planeY * cameraX;
 
-    // -------- DDA ---------
-
-    // get player coords;
-    const mapX = Math.floor(posX);
-    const mapY = Math.floor(posY);
-
-    // length of ray from one x or y-side to next x or y-side
-    // let deltaDistX = Math.abs(1 / rayDirX);
-    // let deltaDistY = Math.abs(1 / rayDirY);
-    let deltaDistX = Math.sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
-    let deltaDistY = Math.sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
     let perpWallDist;
 
-    // direction of step
-    let stepX;
-    let stepY;
-
-    // length of ray from current position to next x or y-side
-    let sideDistX, sideDistY;
-
-    let hit = false;
-    let side; // 0 if x-axis side was hit 1 when y-axis side was hit
-
-    // calculate step & initial sideDist,
-
-    if (rayDirX < 0) {
-      stepX = -1;
-      sideDistX = (posX - mapX) * deltaDistX;
-    } else {
-      stepX = 1;
-      sideDistX = (mapX + 1 - posX) * deltaDistX;
-    }
-    if (rayDirY < 0) {
-      stepY = -1;
-      sideDistY = (posY - mapY) * deltaDistY;
-    } else {
-      stepY = 1;
-      sideDistY = (mapY + 1 - posY) * deltaDistY;
-    } 
-
-    const hitWalls = [];
-    // Actual DDA
-    while (true) {
-      if (sideDistX < sideDistY) {
-        sideDistX += deltaDistX;
-        mapX += stepX;
-        mapX = Math.floor(mapX);
-        side = 0;
-      } else {
-        sideDistY += deltaDistY;
-        mapY += stepY;
-        mapY = Math.floor(mapY);
-        side = 1;
-      }
-
-      if (map[mapX][mapY] > 0) {
-        let newWall = {
-          mapX, mapY, side,
-          value: map[mapX][mapY],
-        };
-
-        let backWallX = mapX;
-        let backWallY = mapY;
-        let backWallSide;
-
-        if (sideDistX < sideDistY) {
-          // sideDistX += deltaDistX;
-          backWallX += stepX;
-          backWallX = Math.floor(backWallX);
-          backWallSide = 0;
-        } else {
-          // sideDistY += deltaDistY;
-          backWallY += stepY;
-          backWallY = Math.floor(backWallY);
-          backWallSide = 1;
-        }
-
-        let backWall = {
-          mapX: backWallX,
-          mapY: backWallY,
-          side: backWallSide,
-          value: 3,
-        };
-
-        newWall.backWall = backWall;
-
-        hitWalls.push(newWall);
-
-        if (map[mapX][mapY] !== 3) {
-          break;
-        }
-      }
-    }
-
-    // ------- DDA DONE -------
+    const [hitWalls, stepX, stepY] = DDA(rayDirX, rayDirY);
 
     // Calculate distance projected on camera
     hitWalls.reverse().forEach(({mapX, mapY, side, value, backWall}) => {
@@ -183,49 +167,8 @@ const update = () => {
         drawStart = h / 2 + lineHeight / 2;
         drawEnd = drawStart - lineHeight / value;
       }
-//
-      // if (value === 2) {
-        // drawStart = h / 2 + lineHeight / 2;
-        // drawEnd = drawStart - lineHeight * value * 1.2;
-      // }
-      // let drawEnd = drawStart - 5 * 10 + 20;
-      // if (value === 3) {
-      // }
 
-      // let color;
-      // switch(map[mapX][mapY]) {
-        // case(1): {
-          // color= 'red';
-          // if (side === 1) {
-            // color = 'salmon';
-          // }
-          // break;
-        // }
-        // case(2): {
-          // color= 'green';
-          // if (side === 1) {
-            // color = 'springgreen';
-          // }
-          // break;
-        // }
-        // case(3): {
-          // color= 'blue';
-          // if (side === 1) {
-            // color = 'skyblue';
-          // }
-          // break;
-        // }
-        // case(4): {
-          // color= 'white';
-          // if (side === 1) {
-            // color = 'whitesmoke';
-          // }
-          // break;
-        // }
-      // }
-
-
-      let wallX
+      let wallX;
       if (side === 0) {
         wallX = posY + perpWallDist * rayDirY;
       } else {
@@ -388,7 +331,7 @@ const loadAsset = (src) => {
 
 loadAsset('Wall.png').then((asset) => {
   image = asset;
-  textureImageData = getImageData(image);
+  let textureImageData = getImageData(image);
   return loadAsset('Floor.png')
 }).then(asset => {
   floorImageData = getImageData(asset)
